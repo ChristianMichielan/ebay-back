c9c7a003bff857294934fa912566ff8d13221eb2
Quentin <quentin.douris63@gmail.com>
2021-11-18 08:48:37 +0100

    ajouts des routes pour l'écran 'mes enchères'

--- routes/utilisateur.js
+++ routes/utilisateur.js
@@ -43,16 +43,29 @@ router.post('/:idUtilisateur/photo', upload.single('avatar'), (req, res) => {
   utilisateurController.mettreAJourPhoto(req.file.path, req, res);
 });
 
-
-
 /**
+ * api/v1/utilisateur/{idUtilisateur}/bien/avendre
  * Retourne les biens vendus (qui sont à livrer), non vendus et livrés à l'acheteur
  */
-router.get('/:idUtilisateur/bien', function (req, res, next) {
+router.get('/:idUtilisateur/bien/avendre', function (req, res, next) {
   bienController.getBiensVendus(req, res, next);
 })
 
+/**
+ * api/v1/utilisateur/{idUtilisateur}/bien/encours
+ * Retourne les biens sur lesquelles l'utilisateur à réaliser une encère et qui sont encore en cours.
+ */
+router.get('/:idUtilisateur/bien/encours', function (req, res, next) {
+  bienController.getEncheresEnCours(req, res, next);
+})
 
+/**
+ * api/v1/utilisateur/{idUtilisateur}/bien/livraisons
+ * Retourne les biens sur lesquelles l'utilisateur a remporté l'enchère ou qui lui ont été livré.
+ */
+router.get('/:idUtilisateur/bien/livraisons', function (req, res, next) {
+  bienController.getLivraisons(req, res, next);
+})
 
 /**
  * api/v1/{idUtilisateur}/bien
